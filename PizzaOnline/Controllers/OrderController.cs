321c466325831841e28adfe6e04cfc359bc31bb7
sheebasalomi <38045937+sheebasalomi@users.noreply.github.com>
2018-11-29 15:48:09 -0600

    Nov 29th Update. New page added 'Build your Pizza'. Created Model-Pizza, Updated Site.css

---
+++ PizzaOnline/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzaOnline.Models;
+
+namespace PizzaOnline.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult BuildYourPizza()
+        {
+            return View();
+        }
+    }
+}
---
+++ PizzaOnline/Models/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaOnline.Models
+{
+    public class Pizza
+    {
+        public int id { get; set; }
+        public string Crust { get; set; }
+        public string Size { get; set; }
+        public string Sauce { get; set; }
+        public string Topping { get; set; }
+    }
+}
